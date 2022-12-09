e7b21c3cf1a070f809af1a661e08cd45e777e550
chandubhargav <110049878+chandubhargav921@users.noreply.github.com>
2022-12-10 00:13:44 +0530

    Add files via upload

---
+++ constructor_oops.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+
+using namespace std;
+
+struct point{
+    int x;
+    int y;
+    point(int xc, int yc){
+        x=xc;
+        y=yc;
+        
+    };
+};
+
+int main()
+{
+    point p(10,15);
+    cout<<p.x<<endl;
+    cout<<p.y<<endl;
+    p.x=20;
+    cout<<p.x<<endl;
+    return 0;
+}
