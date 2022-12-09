4cee1334f5c0438b81aaa6a7b57b91e4b8b60703
chandubhargav <110049878+chandubhargav921@users.noreply.github.com>
2022-12-10 00:12:06 +0530

    Add files via upload

---
+++ iterators_invectors_withmorefunctions.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+#include <algorithm>
+#include <vector>
+
+using namespace std;
+
+int main()
+{
+ vector <int> v={10,20,30,40,50};
+ 
+ vector<int>::iterator i;
+ i=v.begin();
+ cout<<(*i)<<endl;
+ i=next(i);
+ cout<<(*i)<<endl;
+ i=v.end();
+ i=prev(i);
+ cout<<(*i)<<endl;
+ i=prev(i,2);
+ cout<<(*i)<<endl;
+ 
+ return 0;
+ 
+}
