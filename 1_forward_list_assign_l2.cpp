f53c667f49c92df373d20b5188dc2a1ae95c9565
chandubhargav <110049878+chandubhargav921@users.noreply.github.com>
2022-12-11 17:19:12 +0530

    Add files via upload

---
+++ 1_forward_list_assign_l2.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+#include <forward_list>
+using namespace std;
+
+int main()
+{
+    forward_list<int> l;
+    l.assign({10,20,30,40});
+    for(int x:l){
+        cout<<x<<" ";
+    }
+    l.remove(40);
+    cout<<endl;
+    for(int x:l){
+        cout<<x<<" ";
+    }
+    cout<<endl;
+    forward_list<int> l2;
+    l2.assign(l.begin(),l.end());
+    for(int x:l2){
+        cout<<x<<" ";
+    }
+    cout<<endl;
+    forward_list<int> l3;
+    l3.assign(5,10);
+    for(int x:l3){
+        cout<<x<<" ";
+    }
+}
