ee9a71090675030dec0e2062962cdc82311c139a
chandubhargav <110049878+chandubhargav921@users.noreply.github.com>
2022-12-11 17:24:14 +0530

    Add files via upload

---
+++ 1_list_sort_and_reverse.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+#include <list>
+using namespace std;
+
+int main()
+{
+ list<int> l={1000,250,200,0,50,100,60,500};
+  for(int x:l){
+     cout<<x<<" ";
+ }
+  cout<<endl;
+    l.sort();
+   
+     for(int x:l){
+     cout<<x<<" ";
+ }
+ l.reverse();
+  cout<<endl;
+    for(int x:l){
+     cout<<x<<" ";
+ }
+ 
+}
