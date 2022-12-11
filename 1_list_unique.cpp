7a162edfa5b0cdc661434ad76ab6772795d7a545
chandubhargav <110049878+chandubhargav921@users.noreply.github.com>
2022-12-11 17:17:32 +0530

    Add files via upload

---
+++ 1_list_unique.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <list>
+using namespace std;
+
+int main()
+{
+ list<int> l={100,200,200,300,400,400,400,500};
+  for(int x:l){
+     cout<<x<<" ";
+ }
+  cout<<endl;
+    l.unique();
+   
+     for(int x:l){
+     cout<<x<<" ";
+ }
+}
