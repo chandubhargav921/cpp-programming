8b561eda11e61164b43f0f2aa2da84a75a0cbcda
chandubhargav <110049878+chandubhargav921@users.noreply.github.com>
2022-12-12 18:45:56 +0530

    Add files via upload

---
+++ 1_forward_list_merge.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <forward_list>
+using namespace std;
+
+int main()
+{
+    forward_list<int> l1;
+    l1.assign({10,20,30,40});
+     forward_list<int> l2;
+     l2.assign({1,2,3,4});
+     l1.merge(l2);
+     for(int x:l1){
+        cout<<x<<" ";
+    }
+    
+}
