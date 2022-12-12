47be377b9b42ae6beef55795c50e5a9ab64b4578
chandubhargav <110049878+chandubhargav921@users.noreply.github.com>
2022-12-12 18:46:35 +0530

    Add files via upload

---
+++ 1_forward_list_empty.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+#include <forward_list>
+using namespace std;
+
+int main()
+{
+    forward_list<int> l;
+    l.assign({10,20,30,40});
+    l.clear();
+    cout<<l.empty()<<endl;
+}
