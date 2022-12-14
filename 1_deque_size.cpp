877a06818453c6149ffef3eb3cd4d97981e339e8
chandubhargav <110049878+chandubhargav921@users.noreply.github.com>
2022-12-14 14:45:36 +0530

    Add files via upload

---
+++ 1_deque_size.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <deque>
+
+using namespace std;
+
+int main()
+{deque<int> dq={10,20,30};
+auto itr=dq.begin();
+itr++;
+dq.insert(itr,{1,2,3,4,5});
+for(int x:dq){
+    cout<<x<<" ";
+}
+cout<<endl;
+cout<<dq.size();
+}
