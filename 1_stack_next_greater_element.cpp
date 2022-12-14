359b3d9601b6d26c55f742271809bbcadefd46b0
chandubhargav <110049878+chandubhargav921@users.noreply.github.com>
2022-12-14 14:44:22 +0530

    Add files via upload

---
+++ 1_stack_next_greater_element.cpp
@@ -0,0 +1,50 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+int main()
+{vector<int> v;
+int *arr;
+int count=0;
+    cout<<"enter the number of elements"<<endl;
+    int n;
+    int i;
+    cin>>n;
+    int grt;
+    // cout<<"hello";
+    // int i=0;
+    arr=new int(n);
+    for(int i=0;i<n;i++){
+        cin>>arr[i];
+        v.push_back(arr[i]);
+        
+        
+    }
+    reverse(v.begin(),v.end());
+    
+    // cout<<"hello"<<endl;
+    // cout<<v.front()<<endl;
+    for(auto itr=v.begin();itr<=(--v.end());itr++){
+        int k=*(itr);
+        //  itr=v[k];
+        count=0;
+        grt=0;
+        for(auto itrr=v.begin();itrr<itr;itrr++){
+            while(*(itrr)>*(itr)){
+              
+                grt=*(itrr);
+                itrr++;
+                
+            }
+            
+            
+        }
+        if(grt!=0){
+         cout<<grt<<endl;
+        }else{
+            cout<<"-1"<<endl;
+        }
+    }
+}
