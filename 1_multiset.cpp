925ba754db197d5d6c7af3191b3d09589d20d4cb
chandubhargav <110049878+chandubhargav921@users.noreply.github.com>
2022-12-18 20:26:57 +0530

    Add files via upload

---
+++ 1_multiset.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+#include <set>
+
+using namespace std;
+
+int main()
+{
+    multiset<int> ms;
+    ms.insert(10);
+    ms.insert(20);
+    ms.insert(40);
+    ms.insert(10);
+    for(auto x:ms){
+        cout<<x<<" ";
+    }
+    ms.erase(ms.find(10));
+    cout<<endl;
+     for(auto x:ms){
+        cout<<x<<" ";
+    }
+    cout<<endl;
+    ms.insert(10);
+     for(auto x:ms){
+        cout<<x<<" ";
+    }
+    cout<<endl;
+    ms.erase(10);
+     for(auto x:ms){
+        cout<<x<<" ";
+    }
+}
