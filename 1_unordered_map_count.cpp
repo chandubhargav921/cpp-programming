cc057edf3f4e04de04db2e6d348b0d2a0e939f5a
chandubhargav <110049878+chandubhargav921@users.noreply.github.com>
2022-12-27 18:37:13 +0530

    Add files via upload

---
+++ 1_unordered_map_count.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+#include <unordered_map>
+
+using namespace std;
+
+int main()
+{
+    unordered_map<string,int> m;
+    m["chandu"]=143;
+    m.insert({"bhargav",14367});
+    m.insert({"hello",100});
+    auto it=m.find("hello");
+    if(it!=m.end()){
+        cout<<it->second<<endl;
+    }else{
+        cout<<"not found"<<endl;
+    }
+    if(m.count("hello")){
+        cout<<"element found"<<endl;
+    }else{
+        cout<<"not found"<<endl;
+    }
+    for(auto x:m){
+        cout<<x.first<<" "<<x.second;
+        cout<<endl;
+    }
+}
