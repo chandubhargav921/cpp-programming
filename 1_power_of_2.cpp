8f2f8e825d70bbb4ac0b944d9f40adea89a3b7a2
chandubhargav <110049878+chandubhargav921@users.noreply.github.com>
2023-01-08 11:03:19 +0530

    Add files via upload

---
+++ 1_power_of_2.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+int main(){
+    int n;
+    cout<<"enter the value of n"<<endl;
+    cin>>n;
+   if(n%2==0){
+       cout<<"yes"<<endl;
+   }else{
+       cout<<"no"<<endl;
+   }
+}
