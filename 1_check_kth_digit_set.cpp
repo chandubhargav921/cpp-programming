dd69e4e3d816c0ce822f366e17e6eb8ecf719a21
chandubhargav <110049878+chandubhargav921@users.noreply.github.com>
2023-01-08 10:18:10 +0530

    Add files via upload

---
+++ 1_check_kth_digit_set.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+int main(){
+    int n;
+    cout<<"enter the value of n"<<endl;
+    cin>>n;
+    int k;
+    cout<<"enter the value of k"<<endl;
+    cin>>k;
+    int y=(x<<(k-1));
+    if(y-1%2==0){
+        cout<<"the kth digit is set"<<end;
+    }else{
+        cout<<"the kth digit is not set"<<endl;
+    }
+
+    return 0;
+}
