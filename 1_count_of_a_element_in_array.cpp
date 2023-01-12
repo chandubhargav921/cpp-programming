22be52342dd754e918eaaeca6335c40d48d533ce
chandubhargav <110049878+chandubhargav921@users.noreply.github.com>
2023-01-12 23:34:53 +0530

    Add files via upload

---
+++ 1_count_of_a_element_in_array.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+
+using namespace std;
+int check(int n,int arr[],int x){
+    int count=0;
+    for(int i=0;i<n;i++){
+        if(arr[i]==x){
+            count++;
+            
+        }
+    }
+    return count;
+}
+int main()
+{
+    int n,x;
+    int arr[10]={1,10,10,10,20,20,40};
+    cout<<"enter the element to check"<<endl;
+    cin>>x;
+    n=sizeof(arr)/sizeof(1);
+    cout<<check(n,arr,x)<<endl;
+  
+    
+}
