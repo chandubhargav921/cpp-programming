1dc5c328286122067541fa422e84221f3318b2b2
chandubhargav <110049878+chandubhargav921@users.noreply.github.com>
2023-01-12 23:44:41 +0530

    Add files via upload

---
+++ 1_count_no_of_ones.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+#include <vector>
+using namespace std;
+int count(int arr[],int n){
+    int countt=0;
+    vector<int> v(arr,arr+n);
+    auto itr=v.end();
+    for(auto itrr=v.begin();itrr<itr;itrr++){
+        if(*(itrr)==1){
+            countt++;
+        }
+    }
+    return countt;
+}
+int main(){
+   int arr[7]={1,1,1,1,1};
+   cout<<count(arr,7)<<endl;
+    return 0;
+}
