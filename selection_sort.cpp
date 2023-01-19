22920462b128f3c750d5a615029c6b52e2420ec5
chandubhargav <110049878+chandubhargav921@users.noreply.github.com>
2023-01-19 23:58:17 +0530

    Update selection_sort.cpp

--- selection_sort.cpp
+++ selection_sort.cpp
@@ -1,45 +1,67 @@
 #include <bits/stdc++.h>
 using namespace std;
-vector<int> v3;
-int formin(vector<int> v,int n){
-    int min=0;
-   for(int i=0;i<n;i++){
-   if(v[i]<min){
-    min=v[i];
+int minelement(vector<int> v1){
+    int min;
+    for(int i=0;i<v1.size();i++){
+   if(v1[i]<min){
+    min=v1[i];
    }
    }
    return min;
 }
-void answer(vector<int>v,int n){
-    vector<int>::iterator itr = find(v.begin(),v.end(),formin(v,n));
-   v3.push_back(formin(v,n));
-    v.erase(itr);    
-}
-int main(){
-    int *arr,n;
-    cout<<"enter the value of n"<<endl;
-    cin>>n;
-    arr=new int[n];
-    for(int i=0;i<n;i++){
-        cin>>arr[i];
-    }
-   vector<int> v(arr,arr+n);
-//    int min=0;
-//    for(int i=0;i<n;i++){
-//    if(v[i]<min){
-//     min=v[i];
-//    }
-   
-//    auto itr = find(v.begin(),v.end(),formin(v,n));
+int main()
+{
+//     vector<int> v={1,2,3,4,5};
+//     auto itr=find(v.begin(),v.end(),4);
 //     v.erase(itr);
-//     n=v.size();
-while(v.size()!=0){
-    answer(v,n);
-    n=v.size();
-}
-for(int i=0;i<v3.size();i++){
-    cout<<v3[i]<<" ";
-}
 
+// for(auto x:v){
+//     cout<<x<<" ";
+// }
+// int minelement(vector<int> v1){
+//     int min;
+//     for(int i=0;i<n;i++){
+//   if(v1[i]<min){
+//     min=v1[i];
+//   }
+//   }
+//   return min;
+// }
+int *arr,n;
+cout<<"enter the size n"<<endl;
+cin>>n;
+arr = new int[n];
+for(int i=0;i<n;i++){
+    cin>>arr[i];
+}
+vector<int> v2;
+vector<int> v1(arr,arr+n);
+//   while(v1.size()!=0){
+//       int min=0;
+//   for(int i=0;i<n;i++){
+//   if(v1[i]<min){
+//     min=v1[i];
+//   }
+//   }
+// //   vector<int> v2;
+//   v2.push_back(min);
+//   auto itr=find(v1.begin(),v1.end(),min);
+//   v1.erase(itr);
+//   }
+// //  
+// cout<<v2.front();
+cout<<minelement(v1)<<endl;
+while(v1.size()!=0){
+    // int min=minelement(v1);
+auto itr=find(v1.begin(),v1.end(),minelement(v1));
+v2.push_back(*itr);
+v1.erase(itr);
+
+}
+cout<<"the order is"<<endl;
+for(auto x:v2){
+    cout<<x<<" ";
+}
+   
     return 0;
 }
