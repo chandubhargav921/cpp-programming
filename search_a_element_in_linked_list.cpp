43b1a4a4fe6b2ae32714bfe7910da240abf1913c
chandubhargav <110049878+chandubhargav921@users.noreply.github.com>
2023-02-23 01:33:35 +0530

    Add files via upload

---
+++ search_a_element_in_linked_list.cpp
@@ -0,0 +1,79 @@
+#include <iostream>
+
+using namespace std;
+struct node{
+    int data;
+    node *next;
+    node(int x){
+        data=x;
+        next=NULL;
+    }
+};
+
+int main()
+{
+    node *head=new node(10);
+    head->next=new node(20);
+    node *temp=head->next;
+    temp->next=new node(35);
+    node *temp2=head;
+    head=new node(5);
+    head->next=temp2;
+    // node *temp1=head;
+    // while(temp1->next->next!=NULL){
+    //     temp1=temp1->next;
+    // }
+    // // delete at last 
+    // temp1->next=NULL;
+    // node *temp6=temp1->next;
+    node *temp10=head;
+    while(temp10->next!=NULL){
+        cout<<temp10->data<<" ";
+        temp10=temp10->next;
+    }
+    cout<<temp10->data;
+    // temp1->next=new node(60);
+    
+    node *temp3=head;
+    int i=0;
+    while(temp3->next!=NULL){
+        // cout<<temp3->data<<" ";
+        temp3=temp3->next;
+        i++;
+        if(i==3){
+            break;
+        }
+    }
+    node *temp8 = temp3->next;
+    temp3->next=new node(80);
+temp3->next->next=temp8;
+    // cout<<temp3->data<<endl;
+    // cout<<head->next->data;
+    cout<<endl;
+    node *temp11=head;
+    while(temp11->next!=NULL){
+        cout<<temp11->data<<" ";
+        temp11=temp11->next;
+    }
+    cout<<temp11->data;
+    // cout<<endl;
+    // // delete first node 
+    
+    // node *temp5=head->next;
+    // head->next=NULL;
+    // head=temp5;
+    // cout<<head->data;
+    cout<<endl;
+    node *temp12=head;
+    int j=1;
+    while(temp12->next!=NULL){
+        if(temp12->data==20){
+            cout<<i<<endl;
+            break;
+        }
+        temp12=temp12->next;
+    }
+   
+   
+    return 0;
+}
