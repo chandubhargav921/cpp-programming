a3932d8ca5af0d0967b2e87707de83ffaa2e7d12
chandubhargav <110049878+chandubhargav921@users.noreply.github.com>
2023-03-04 19:57:52 +0530

    Add files via upload

---
+++ adjacency_matrix_graph.cpp
@@ -0,0 +1,25 @@
+#include <bits/stdc++.h>
+using namespace std;
+void addEdge(vector<int> adj[],int u,int v){
+    adj[u].push_back(v);
+    adj[v].push_back(u);
+}
+void printGraph(vector<int> adj[],int v){
+    for(int i=0;i<v;i++){
+        for(int x:adj[i]){
+            cout<<x<<" ";
+
+        }
+        cout<<endl;
+    }
+}
+int main(){
+    int v=4;
+    vector<int> adj[v];
+    addEdge(adj,0,1);
+    addEdge(adj,0,2);
+    addEdge(adj,1,2);
+    addEdge(adj,1,3);
+    printGraph(adj,4);
+    return 0;
+}
